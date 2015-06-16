1d533b5f9ff5226b5ea698c75ab74f6252ee744f
info@netarchitectures.co.uk <info@netarchitectures.co.uk>
2015-06-16 10:24:07 +0200

    Basic House object created

---
+++ Ltd.NA.Emlak/Core/Ltd.NA.Emlak.Domain/House.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ltd.NA.Emlak.Domain
+{
+    public class House
+    {
+        private Guid id;
+        private string name;
+        private string description;
+
+
+        public Guid Id
+        {
+            get { return this.id; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+        }
+
+        public bool IsValid
+        {
+            get { return this.id != Guid.Empty; }
+        }
+
+        public static House Create(Guid id, string name, string description)
+        {
+            return new House
+            {
+                id = id,
+                name = name,
+                description = description
+            };
+        }
+    }
+}
---
+++ Ltd.NA.Emlak/Tests/Ltd.NA.Emlak.Domain.Tests/HouseTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ltd.NA.Emlak.Domain.Tests
+{
+    [TestClass]
+    public class HouseTests
+    {
+        [TestMethod]
+        public void House_MustHave_Name_And_Description()
+        {
+            House house = House.Create(Guid.NewGuid(), "My Name", "My Description");
+
+            Assert.IsTrue(house.Id != Guid.Empty);
+            Assert.IsTrue(house.Name == "My Name");
+            Assert.IsTrue(house.Description == "My Description");
+        }
+    }
+}

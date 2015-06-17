3cb3934863866be2382cdca312bd3f1fefa39a34
mrtKaratepe <mrt.karatepe@hotmail.com>
2015-06-17 12:02:38 +0300

    Address and Category Classes Commit2

---
+++ Ltd.NA.Emlak/Core/Ltd.NA.Emlak.Domain/Address.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ltd.NA.Emlak.Domain
+{
+    public class Address
+    {
+        private string adressLine1 { get; set; }
+        private string adressLine2 { get; set; }
+        private int number { get; set; }
+        private string zipCode { get; set; }
+        private string city { get; set; }
+        private string country { get; set; }
+        private string county { get; set; }
+
+        
+
+
+        public static Address Create(string adressLine1, int number, string city, string country)
+        {
+            return new Address
+            {
+                adressLine1 = adressLine1,
+                number = number,
+                city = city,
+                country = country
+            };
+        }
+    }
+}
---
+++ Ltd.NA.Emlak/Core/Ltd.NA.Emlak.Domain/Category.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ltd.NA.Emlak.Domain
+{
+    public class Category
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private string entry;
+        private string description;
+
+        public string Entry
+        {
+            get { return this.entry; }
+        }
+
+        public string Description
+        {
+            get { return this.description; }
+        }
+
+        public static Category Create(string entry, string description)
+        {
+            return new Category 
+            {
+                entry=entry,
+                description=description
+
+            };
+        }
+
+        
+    }
+}

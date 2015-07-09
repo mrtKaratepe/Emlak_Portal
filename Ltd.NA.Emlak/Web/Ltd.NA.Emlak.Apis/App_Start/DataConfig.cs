d500618abe3b94dfd118371a63f431120e496149
mrtKaratepe <mrt.karatepe@hotmail.com>
2015-07-09 03:33:00 +0300

    .More Test Data added .aaplicatioBoot paging completed .search navbar fully functional .navbar next and previous bugs are fixed

--- Ltd.NA.Emlak/Web/Ltd.NA.Emlak.Apis/App_Start/DataConfig.cs
+++ Ltd.NA.Emlak/Web/Ltd.NA.Emlak.Apis/App_Start/DataConfig.cs
@@ -18,9 +18,22 @@ namespace Ltd.NA.Emlak.Apis
                 Customer customer = Customer.Create("First Name", "Last Name", 43, "12345ABCDE");
 
                 House firstHouse = House.Create("First house", "Description for the house", customer);
-                firstHouse.AddAddress("100/1 Sokak", "Izmir", "Turkey", "23A", "35600");
+                firstHouse.AddAddress("111/1 Sokak", "Izmir", "Turkey", "23A", "35600");
+                //---
+                Customer customer2 = Customer.Create("First Name", "Last Name", 43, "12345ABCDE");
+
+                House secondHouse = House.Create("First house", "Description for the house", customer2);
+                secondHouse.AddAddress("222/1 Sokak", "Izmir", "Turkey", "23A", "35600");
+                //---
+                Customer customer3 = Customer.Create("First Name", "Last Name", 43, "12345ABCDE");
+
+                House thirdHouse = House.Create("First house", "Description for the house", customer3);
+                thirdHouse.AddAddress("333/1 Sokak", "Izmir", "Turkey", "23A", "35600");
+
 
                 context.Customers.Add(customer);
+                context.Customers.Add(customer2);
+                context.Customers.Add(customer3);
                 context.SaveChanges();
             }
         }
--- Ltd.NA.Emlak/Web/Ltd.NA.Emlak.Website/Scripts/applicationBoot.js
+++ Ltd.NA.Emlak/Web/Ltd.NA.Emlak.Website/Scripts/applicationBoot.js
@@ -13,18 +13,24 @@ emlakApp.controller('searchController', function ($scope, $http, $q) {
         city: undefined,
         zipCode: undefined,
         type: undefined,
-        take: 10,
+        take: 2,
         skip: 0
     };
 
     // paging view model
     $scope.paging = {
         totalRows: 0,
+        recordsPerPage: $scope.filter.take,
         totalPages: function () {
-            return new Array(Math.round(this.totalRows / this.recordsPerPage));
+            var range = new Array ;
+            var total = Math.ceil(this.totalRows / this.recordsPerPage);
+
+            for (var i = 0; i <total; i++) {
+                range.push(i);
+            }
+            return range;
         },
-        currentPage: 0,
-        recordsPerPage: $scope.filter.take
+        currentPage: 0
     };
 
     $scope.search = function gethouses() {
@@ -51,11 +57,20 @@ emlakApp.controller('searchController', function ($scope, $http, $q) {
     };
 
     $scope.loadRows = function (page) {
-        // clean up the collection
+        var totalPagesCount = Math.ceil($scope.paging.totalRows / $scope.paging.recordsPerPage);
+        if (totalPagesCount > page && page >= 0) {
+            // clean up the collection
+            $scope.loading = true;
+            this.houses = [];
+            $scope.filter.skip = $scope.filter.take * page;
 
-        // send a new search request with new parameters
+            // send a new search request with new parameters
+            $scope.search();
 
-        // update the paging view model
+            // update the paging view model
+            $scope.paging.currentPage = page;
+            $scope.loading = false;
+        }
         console.log(page);
     };
 });
